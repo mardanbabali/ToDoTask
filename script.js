283c6caa27d93c1696ec432281247f067e3707c0
Mardan <mardanvb@code.edu.az>
2023-06-07 01:59:27 +0400

    todolist

---
+++ script.js
@@ -0,0 +1,30 @@
+let list = [];
+let listUl = document.getElementById('row');
+const btn =document.getElementById("btn")
+function todo() {
+  
+    let input = document.getElementById('input');
+    let value = input.value.trim();
+
+    if (value) {
+        list.push(value);
+        Display();
+        input.value = "";
+    } else {
+        alert("Deyer daxil edin");
+    }
+}
+
+function Delete(index) {
+    list.splice(index, 1);
+    Display();
+}
+
+function Display() {
+    let data = "";
+    for (let i = 0; i < list.length; i++) {
+        data += `<li class="list-group-item d-flex justify-content-between">${list[i]} <button onclick="Delete(${i})" class="btn btn-danger btn-sm">Delete</button></li>`;
+    }
+
+    listUl.innerHTML = data;
+}
